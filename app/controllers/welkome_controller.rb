3842ce61463ec4e6a16618b831bd7a287109f215
Sergio von Borries <svonborries@gmail.com>
2020-03-18 21:16:30 -0400

    Added parameters to the form for the automatic processes

--- app/controllers/welkome_controller.rb
+++ app/controllers/welkome_controller.rb
@@ -4,9 +4,14 @@ class WelkomeController < ApplicationController
     end
 
     def show
-        search_term = params['interval'].capitalize
+        simultaneous = params['simultaneous']
+        time = params['time']
+        repeat_times = params['repeat_times']
         # @response = Faraday.get  "http://localhost:3000/api/v1/orders/#{search_term}"
         # @data = JSON.parse @response.body
+        rand(1..simultaneous).times do 
+
+        end
         url = "http://localhost:3000/api/v1/orders"
         @response = Faraday.post(url, generate_orders.to_json, "Content-Type" => "application/json")
         @data = JSON.parse @response.body
